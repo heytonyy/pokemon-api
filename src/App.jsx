5c3e582f0d9a442e0170e07f491f06c58bc844ff
Tony Aiello <heytonyyy3@gmail.com>
2022-10-26 18:26:24 -0700

    updated mern stack

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './App.module.css'
+import Form from './components/Form'
+
+export default function App() {
+
+  return (
+    <div className={style.App}>
+      <Form />
+    </div>
+  )
+}
